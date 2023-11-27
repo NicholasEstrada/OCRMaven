7626a81419806267ce3eea3b608f8281b3fbefa7
nicholasestrada <nicholas26cervatti@gmail.com>
2023-11-27 18:17:18 -0300

    alterações de encapsulamento e testes com arquivo temporario

--- src/main/java/JavaTCC/OCRMaven/Main.java
+++ src/main/java/JavaTCC/OCRMaven/Main.java
@@ -1,39 +1,25 @@
-package JavaTCC.OCRMaven;
-
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
-public class Main {
-    public static void main(String[] args) {
-        // URL do arquivo temporário na internet
-        String url = "https://araucariageneticabovina.com.br/arquivos/servico/pdfServico_57952bf8ca7af_24-07-2016_17-58-32.pdf";
-
+public class ExtractFileExtension {
+    public static String extractFileExtension(String url) {
         try {
-            // Baixe o arquivo da internet
-            byte[] fileBytes = IOUtils.toByteArray(new URL(url));
-            Path tempFilePath = Files.createTempFile("tempFile", ".pdf");
-            Files.write(tempFilePath, fileBytes);
+            URL urlObj = new URL(url);
+            String path = urlObj.getPath();
+            int lastDotIndex = path.lastIndexOf('.');
 
-            // Use o Tesseract OCR para extrair texto da imagem
-            ITesseract tesseract = new Tesseract();
-            tesseract.setDatapath("C:\\Users\\Nicholas\\git\\OCRMaven\\tessdata");
-            String textoExtraido = tesseract.doOCR(tempFilePath.toFile());
-
-            // Imprima o texto extraído
-            System.out.println(textoExtraido);
-            FileUtils.deleteQuietly(tempFilePath.toFile());
-
-        } catch (IOException | TesseractException e) {
-            e.printStackTrace();
+            if (lastDotIndex != -1) {
+                return path.substring(lastDotIndex);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Trate exceções adequadamente em um ambiente de produção
         }
+
+        return ""; // Se não encontrar a extensão, retorna uma string vazia ou outra indicação apropriada
+    }
+
+    public static void main(String[] args) {
+        String url = "https://araucariageneticabovina.com.br/arquivos/servico/pdfServico_57952bf8ca7af_24-07-2016_17-58-32.pdf";
+        String extension = extractFileExtension(url);
+        System.out.println(extension);
     }
 }
